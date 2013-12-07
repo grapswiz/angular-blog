d2c703b788545817e80255f9cdc6eb6c1b5d90a4
grapswiz <ineeza@gmail.com>
2013-12-07 13:27:52 +0900

    add local server

---
+++ dev_server.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+require 'json'
+
+set :public_folder, File.dirname(__FILE__) + '/'
+
+get '/blog/entries' do
+  content_type :json
+  entries = [
+      {
+          id: "1",
+          title: "title1",
+          tags: ["tag1", "tag2"],
+          content: "月は東に日は西に。月日は百代の過客にして行き交う人々もまた旅人なり。"
+      },
+      {
+          id: "2",
+          title: "title2",
+          tags: ["tag2", "tag3"],
+          content: "A long time ago..."
+      }
+  ]
+  entries.to_json
+end
